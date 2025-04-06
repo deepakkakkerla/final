542db3de90adb2f383c407b19ac351b3a126a3c8
deepakkakkerla <deepakdeepak76099@gmail.com>
2025-04-06 19:36:59 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
-import Admin from "./Pages/Admin";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
+import Product from "./Pages/Product";
+import Footer from "./Components/Footer/Footer";
+import ShopCategory from "./Pages/ShopCategory";
+import women_banner from "./Components/Assets/banner_women.png";
+import men_banner from "./Components/Assets/banner_mens.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
+import LoginSignup from "./Pages/LoginSignup";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
 
 export const backend_url = 'http://localhost:4000';
 export const currency = '₹';
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <div>
+    <div>
+      <Router>
         <Navbar />
-        <Admin />
+        <Routes>
+          <Route path="/" element={<Shop gender="all" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path='/about' element={<About/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+     
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup/>} />
+        </Routes>
         <Footer />
-      </div>
-    </BrowserRouter>
+      </Router>
+    </div>
   );
 }
 
